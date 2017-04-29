ffc8bd09f1687749ab4985470971113a638ac567
John Kelly <benjibobs@users.noreply.github.com>
2017-04-29 15:42:26 +0100

    Update small copyright change

--- Steam Shutdown/Properties/AssemblyInfo.cs
+++ Steam Shutdown/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("benjibobs")]
 [assembly: AssemblyProduct("Steam Shutdown")]
-[assembly: AssemblyCopyright("Copyright © benjibobs 2016")]
+[assembly: AssemblyCopyright("Copyright © benjibobs 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
